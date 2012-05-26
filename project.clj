78f7b6144aabe514b8a789ff7ad205449c751738
Norman Richards <orb@nostacktrace.com>
2012-05-26 18:46:23 -0500

    remove the FIXME

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject euler "1.0.0-SNAPSHOT"
-  :description "FIXME: write description"
+  :description "project euler solutions"
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [org.clojure/math.numeric-tower "0.0.1"]
                  [org.clojure/math.combinatorics "0.0.2"]])
